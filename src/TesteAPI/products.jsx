ea861ded5bedc41273a6d877de63ffa22800f1a5
Jherrerocavadas <jherrerocavadas@gmail.com>
2022-12-13 17:03:25 -0200

    Criação de arquivos de teste para simulação da API

---
+++ src/TesteAPI/products.jsx
@@ -0,0 +1,208 @@
+
+export const products = [
+    {
+        "_id": "6382853b54c75d793b9a1204",
+        "vendedor": "638013bd4004847ffe4e6e3a",
+        "nome": "Pão de Batata",
+        "descricao": "É um pão de batata",
+        "imagePath": "https://www.nigiloc.com/images/image-not-found.png",
+        "preco": 15,
+        "ingredientes": [
+            {
+                "nome": "Pão",
+                "quantidade": 1,
+                "unidadeMedida": "un",
+                "_id": "6382853b54c75d793b9a1205"
+            },
+            {
+                "nome": "Batata",
+                "quantidade": 1,
+                "unidadeMedida": "kg",
+                "_id": "6382853b54c75d793b9a1206"
+            }
+        ],
+        "categoria": "63813b9991622f795ad21ae5",
+        "aceitaEncomenda": false,
+        "variacoesProduto": [
+            {
+                "nome": "Cobertura",
+                "valores": [
+                    {
+                        "valor": "Requeijão",
+                        "_id": "6382853b54c75d793b9a1208"
+                    },
+                    {
+                        "valor": "Cheddar",
+                        "_id": "6382853b54c75d793b9a1209"
+                    }
+                ],
+                "_id": "6382853b54c75d793b9a1207"
+            }
+        ],
+        "__v": 0
+    },
+    {
+        "_id": "638285cc6e27ab60aad7f1e3",
+        "vendedor": "638013bd4004847ffe4e6e3a",
+        "nome": "Ovomaltine",
+        "descricao": "É um pote de ovomaltine, bro",
+        "imagePath": "https://www.nigiloc.com/images/image-not-found.png",
+        "preco": 15,
+        "ingredientes": [
+            {
+                "nome": "Ovo",
+                "quantidade": 1,
+                "unidadeMedida": "sim",
+                "_id": "638285cc6e27ab60aad7f1e4"
+            },
+            {
+                "nome": "Malte",
+                "quantidade": 1,
+                "unidadeMedida": "sim",
+                "_id": "638285cc6e27ab60aad7f1e5"
+            }
+        ],
+        "categoria": "63813b9991622f795ad21ae5",
+        "aceitaEncomenda": true,
+        "variacoesProduto": [
+            {
+                "nome": "Sabor",
+                "valores": [
+                    {
+                        "valor": "Nescau",
+                        "_id": "638285cc6e27ab60aad7f1e7"
+                    },
+                    {
+                        "valor": "Toddy",
+                        "_id": "638285cc6e27ab60aad7f1e8"
+                    }
+                ],
+                "_id": "638285cc6e27ab60aad7f1e6"
+            }
+        ],
+        "__v": 0
+    },
+    {
+        "_id": "638285cc6e11ab60aad7f1e3",
+        "vendedor": "638013bd4004847ffe4e6e3a",
+        "nome": "Ovomaltine2",
+        "descricao": "É um pote de ovomaltine, bro",
+        "imagePath": "https://www.nigiloc.com/images/image-not-found.png",
+        "preco": 15,
+        "ingredientes": [
+            {
+                "nome": "Ovo",
+                "quantidade": 1,
+                "unidadeMedida": "sim",
+                "_id": "638285cc6e27ab60aad7f1e4"
+            },
+            {
+                "nome": "Malte",
+                "quantidade": 1,
+                "unidadeMedida": "sim",
+                "_id": "638285cc6e27ab60aad7f1e5"
+            }
+        ],
+        "categoria": "63813b9991622f795ad21ae5",
+        "aceitaEncomenda": false,
+        "variacoesProduto": [
+            {
+                "nome": "Sabor",
+                "valores": [
+                    {
+                        "valor": "Nescau",
+                        "_id": "638285cc6e27ab60aad7f1e7"
+                    },
+                    {
+                        "valor": "Toddy",
+                        "_id": "638285cc6e27ab60aad7f1e8"
+                    }
+                ],
+                "_id": "638285cc6e27ab60aad7f1e6"
+            }
+        ],
+        "__v": 0
+    },
+    {
+        "_id": "638285cc6e27ab60aad7f1e3",
+        "vendedor": "638013bd4004847ffe4e6e3a",
+        "nome": "Ovomaltine",
+        "descricao": "É um pote de ovomaltine, bro",
+        "imagePath": "https://www.nigiloc.com/images/image-not-found.png",
+        "preco": 15,
+        "ingredientes": [
+            {
+                "nome": "Ovo",
+                "quantidade": 1,
+                "unidadeMedida": "sim",
+                "_id": "638285cc6e27ab60aad7f1e4"
+            },
+            {
+                "nome": "Malte",
+                "quantidade": 1,
+                "unidadeMedida": "sim",
+                "_id": "638285cc6e27ab60aad7f1e5"
+            }
+        ],
+        "categoria": "63813b9991622f795ad21ae5",
+        "aceitaEncomenda": true,
+        "variacoesProduto": [
+            {
+                "nome": "Sabor",
+                "valores": [
+                    {
+                        "valor": "Nescau",
+                        "_id": "638285cc6e27ab60aad7f1e7"
+                    },
+                    {
+                        "valor": "Toddy",
+                        "_id": "638285cc6e27ab60aad7f1e8"
+                    }
+                ],
+                "_id": "638285cc6e27ab60aad7f1e6"
+            }
+        ],
+        "__v": 0
+    },
+    {
+        "_id": "638285cc6e27ab60aad7f1e3",
+        "vendedor": "638013bd4004847ffe4e6e3a",
+        "nome": "Ovomaltine",
+        "descricao": "É um pote de ovomaltine, bro",
+        "imagePath": "https://www.nigiloc.com/images/image-not-found.png",
+        "preco": 15,
+        "ingredientes": [
+            {
+                "nome": "Ovo",
+                "quantidade": 1,
+                "unidadeMedida": "sim",
+                "_id": "638285cc6e27ab60aad7f1e4"
+            },
+            {
+                "nome": "Malte",
+                "quantidade": 1,
+                "unidadeMedida": "sim",
+                "_id": "638285cc6e27ab60aad7f1e5"
+            }
+        ],
+        "categoria": "63813b9991622f795ad21ae5",
+        "aceitaEncomenda": true,
+        "variacoesProduto": [
+            {
+                "nome": "Sabor",
+                "valores": [
+                    {
+                        "valor": "Nescau",
+                        "_id": "638285cc6e27ab60aad7f1e7"
+                    },
+                    {
+                        "valor": "Toddy",
+                        "_id": "638285cc6e27ab60aad7f1e8"
+                    }
+                ],
+                "_id": "638285cc6e27ab60aad7f1e6"
+            }
+        ],
+        "__v": 0
+    }
+]
---
+++ src/TesteAPI/variacoesProduto.jsx
@@ -0,0 +1,184 @@
+export const variacoesProdutoTeste = [
+    {
+        "nome": "Cobertura",
+        "valores": [
+            {
+                "valor": "Requeijão",
+                "_id": "6382853b54c75d793b9a1208"
+            },
+            {
+                "valor": "Cheddar",
+                "_id": "6382853b54c75d793b9a1209"
+            }
+        ],
+        "_id": "6382853b54c75d793b9a1207"
+    },
+
+    {
+        "nome": "Sabor1",
+        "valores": [
+            {
+                "valor": "S",
+                "_id": "6382333b54c75d793b9a1208"
+            },
+            {
+                "valor": "N",
+                "_id": "6382123b54c75d793b9a1209"
+            },
+            {
+                "valor": "11231231231",
+                "_id": "6382123bfdfd5d793b9a1209"
+            },
+            {
+                "valor": "2sdsdssds",
+                "_id": "6382123212asda793b9a1209"
+            },
+            {
+                "valor": "3asasd fd",
+                "_id": "6382123111c75d793b9a1209"
+            },
+            {
+                "valor": "fdfdfdfdfd4",
+                "_id": "6382113b54vvvvvv3b9a1209"
+            },
+            {
+                "valor": "jkjkjkjkjk5",
+                "_id": "638212cvc4c75d793b9a1209"
+            }
+        ],
+        "_id": "6382853b54c722793b9a1207"
+    },
+    {
+        "nome": "Sabor1",
+        "valores": [
+            {
+                "valor": "S",
+                "_id": "6382333b54c75d793b9a1208"
+            },
+            {
+                "valor": "N",
+                "_id": "6382123b54c75d793b9a1209"
+            },
+            {
+                "valor": "11231231231",
+                "_id": "6382123bfdfd5d793b9a1209"
+            },
+            {
+                "valor": "2sdsdssds",
+                "_id": "6382123212asda793b9a1209"
+            },
+            {
+                "valor": "3asasd fd",
+                "_id": "6382123111c75d793b9a1209"
+            },
+            {
+                "valor": "fdfdfdfdfd4",
+                "_id": "6382113b54vvvvvv3b9a1209"
+            },
+            {
+                "valor": "jkjkjkjkjk5",
+                "_id": "638212cvc4c75d793b9a1209"
+            }
+        ],
+        "_id": "6382853b54c722793b9a1207"
+    },{
+        "nome": "Sabor1",
+        "valores": [
+            {
+                "valor": "S",
+                "_id": "6382333b54c75d793b9a1208"
+            },
+            {
+                "valor": "N",
+                "_id": "6382123b54c75d793b9a1209"
+            },
+            {
+                "valor": "11231231231",
+                "_id": "6382123bfdfd5d793b9a1209"
+            },
+            {
+                "valor": "2sdsdssds",
+                "_id": "6382123212asda793b9a1209"
+            },
+            {
+                "valor": "3asasd fd",
+                "_id": "6382123111c75d793b9a1209"
+            },
+            {
+                "valor": "fdfdfdfdfd4",
+                "_id": "6382113b54vvvvvv3b9a1209"
+            },
+            {
+                "valor": "jkjkjkjkjk5",
+                "_id": "638212cvc4c75d793b9a1209"
+            }
+        ],
+        "_id": "6382853b54c722793b9a1207"
+    },{
+        "nome": "Sabor1",
+        "valores": [
+            {
+                "valor": "S",
+                "_id": "6382333b54c75d793b9a1208"
+            },
+            {
+                "valor": "N",
+                "_id": "6382123b54c75d793b9a1209"
+            },
+            {
+                "valor": "11231231231",
+                "_id": "6382123bfdfd5d793b9a1209"
+            },
+            {
+                "valor": "2sdsdssds",
+                "_id": "6382123212asda793b9a1209"
+            },
+            {
+                "valor": "3asasd fd",
+                "_id": "6382123111c75d793b9a1209"
+            },
+            {
+                "valor": "fdfdfdfdfd4",
+                "_id": "6382113b54vvvvvv3b9a1209"
+            },
+            {
+                "valor": "jkjkjkjkjk5",
+                "_id": "638212cvc4c75d793b9a1209"
+            }
+        ],
+        "_id": "6382853b54c722793b9a1207"
+    },{
+        "nome": "Sabor1",
+        "valores": [
+            {
+                "valor": "S",
+                "_id": "6382333b54c75d793b9a1208"
+            },
+            {
+                "valor": "N",
+                "_id": "6382123b54c75d793b9a1209"
+            },
+            {
+                "valor": "11231231231",
+                "_id": "6382123bfdfd5d793b9a1209"
+            },
+            {
+                "valor": "2sdsdssds",
+                "_id": "6382123212asda793b9a1209"
+            },
+            {
+                "valor": "3asasd fd",
+                "_id": "6382123111c75d793b9a1209"
+            },
+            {
+                "valor": "fdfdfdfdfd4",
+                "_id": "6382113b54vvvvvv3b9a1209"
+            },
+            {
+                "valor": "jkjkjkjkjk5",
+                "_id": "638212cvc4c75d793b9a1209"
+            }
+        ],
+        "_id": "6382853b54c722793b9a1207"
+    }
+]
