6b77fe9c0bd58356b18d34c5adfdf1bc59742fae
Jherrerocavadas <jherrerocavadas@gmail.com>
2022-12-13 17:15:14 -0200

    Criação da lista de categorias e produtos dinâmicas

--- src/Components/home/Categories.jsx
+++ src/Components/home/Categories.jsx
@@ -1,39 +1,21 @@
-import React from 'react'
-import {Text, View, Image} from 'react-native'
-import { useFonts } from 'expo-font'
+import Category from "./Category";
+import { FlatList } from "react-native"
+import { categories } from "../../TesteAPI/categories"
 
-export default function Categories({SaaS, texto}){
-
-    const [fontsLoaded] = useFonts({
-        PoppinsLight: require('../../../assets/fonts/Poppins-Light.ttf'),
-        PoppinsRegular: require('../../../assets/fonts/Poppins-Regular.ttf'),
-        PoppinsSemiBold: require('../../../assets/fonts/Poppins-SemiBold.ttf'),
-        PoppinsBold: require('../../../assets/fonts/Poppins-Medium.ttf'),
-        PoppinsBlack: require('../../../assets/fonts/Poppins-Bold.ttf'),
-
-    })
-
-    const viewStyles = {
-        backgroundColor:'#d1d1d1', 
-        width:70, 
-        height:70, 
-        borderRadius:100, 
-        overflow:'hidden', 
-        borderColor:'#FF881D', 
-        borderWidth:2, 
-        marginLeft:10
-    }
-
+export default function Categories(){
     return(
-
-        <View style={{width:100, alignItems:'center'}}>
-                <View style={{...viewStyles}}>
-                    <Image source={{uri:SaaS}} 
-                        style={{flex:1, width:null, height:null, resizeMode:'cover'}}
-                    />
-                </View>
-            <Text style={{fontSize:16, color:'black',marginLeft:10, fontFamily:'PoppinsRegular'}}>{texto}</Text>
-        </View>
-
-    )
+          <FlatList
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            decelerationRate={'fast'}
+            data={categories}
+            keyExtractor={category => category._id}
+            renderItem={({item : category}) => (
+              <Category
+                 urlCategory={category.imagePath}
+                 texto={category.nome}
+               />
+             )}
+            />
+        )
 }
---
+++ src/Components/home/Category.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import {Text, View, Image} from 'react-native'
+import { useFonts } from 'expo-font'
+
+export default function Categories({urlCategory, texto}){
+
+    let [fontsLoaded] = useFonts({
+        PoppinsLight: require('../../../assets/fonts/Poppins-Light.ttf'),
+        PoppinsRegular: require('../../../assets/fonts/Poppins-Regular.ttf'),
+        PoppinsSemiBold: require('../../../assets/fonts/Poppins-SemiBold.ttf'),
+        PoppinsBold: require('../../../assets/fonts/Poppins-Medium.ttf'),
+        PoppinsBlack: require('../../../assets/fonts/Poppins-Bold.ttf'),
+
+    })
+
+    const viewStyles = {
+        backgroundColor:'#d1d1d1',
+        width:70,
+        height:70,
+        borderRadius:100,
+        overflow:'hidden',
+        borderColor:'#FF881D',
+        borderWidth:2,
+        marginLeft:10
+    }
+
+    return(
+
+        <View style={{width:100, alignItems:'center'}}>
+                <View style={{...viewStyles}}>
+                    <Image source={{uri:urlCategory}}
+                        style={{flex:1, width:null, height:null, resizeMode:'cover'}}
+
+                    />
+                </View>
+            <Text style={{fontSize:16, color:'black',marginLeft:10, fontFamily:'PoppinsRegular'}}>{texto}</Text>
+        </View>
+
+    )
+}
